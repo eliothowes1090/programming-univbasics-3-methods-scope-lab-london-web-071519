3e714793ca79e073baca6526b5303dad920600ed
Eliot Howes <ejhowes@gmail.com>
2019-06-05 13:55:37 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,3 +1,5 @@
+phrase = "It's-a me, Mario!"
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
@@ -6,5 +8,3 @@ end
 def toadstool
   puts status
 end
-
-phrase = "It's-a me, Mario!"
